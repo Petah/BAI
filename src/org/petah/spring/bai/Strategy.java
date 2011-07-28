0bea8dbb32720ae4f4df6c10f006e741ed68d49c
Petah <Petah.P@gmail.com>
2011-07-28 14:11:49 +1200

    remove redundant comment, format

--- src/org/petah/spring/bai/Strategy.java
+++ src/org/petah/spring/bai/Strategy.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.petah.spring.bai;
 
 /**
@@ -12,4 +8,5 @@ public enum Strategy {
 
     Rush, Defend, Assault, Tech,
     Kbot, Vehicle, Aircraft, Hovercraft, Sea, Seaplanes
+
 }
